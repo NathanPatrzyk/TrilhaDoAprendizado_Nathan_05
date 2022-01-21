db6417aa86e11ca64c0eb9824a4e80d03e991a9b
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2022-01-21 16:55:39 -0300

    4.0.3.7

--- exercicio10.js
+++ exercicio10.js
@@ -1,24 +1,42 @@
 var nome = prompt("Digite seu nome : ")
 
 var valorSecreto = 7
+
 var minimo = 2
 var maximo = 10
-
-var valor = prompt("Digite um número")
 
 var espaco = ("<p>------------------------------------------------------------</p>")
 
 var acerto = ("<p>"+nome+" você acertou!!!</p>")
-var erro = (nome+" você acertou!!!")
 
-var mensagemValor = ("Digite um número entre "+minimo+" e "+maximo+" : ")
+var erro = (nome+" você errou!!!")
 
-if(valor == valorSecreto){
-    document.write(espaco)
-    document.write(acerto)
-    document.write(espaco)
+var contadorA = 0
+
+var valor = prompt("Digite um número entre "+minimo+" e "+maximo+" : ")
+
+for(contadorA=1; contadorA<4; contadorA++){
+
+    if(valor == valorSecreto){
+        document.write(espaco)
+        document.write(acerto+" O número correto é 7.")
+        document.write(espaco)
+        contadorA=999
+
+    }
+
+    else{
+        minimo++
+        maximo--
+
+
+        alert(erro)
+        var valor = prompt("Digite um número entre "+minimo+" e "+maximo+" : ")
+    }
 }
-else{
-    alert(erro)
-    var valor = prompt("Digite um número entre "+minimo+" e "+maximo+" : ")
+
+if(contadorA == 4){
+    document.write(espaco)
+    document.write("<p>"+erro+" O número correto é 7.</p>")
+    document.write(espaco)
 }
