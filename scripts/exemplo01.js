0b71a9a6eec5282a4eef15c6c0087487f308ae0a
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2022-02-17 15:25:54 -0300

    5.3

--- scripts/exemplo01.js
+++ scripts/exemplo01.js
@@ -1,18 +1,21 @@
-var primeiroNumero=parseInt(prompt("Digite um número:"))
-var segundoNumero=parseInt(prompt("Digite outro número:")) 
+function calcular(){
+	var primeiroNumero = document.getElementById("primeiroNumero").value
+	var segundoNumero = document.getElementById("segundoNumero").value
 
-if(primeiroNumero>segundoNumero){
+	if(primeiroNumero>segundoNumero){
 	document.write("<h2>"+primeiroNumero+" é maior que "+segundoNumero+"</h2>")
-}
+	}
 
-else if(segundoNumero>primeiroNumero){
+	else if(segundoNumero>primeiroNumero){
 	document.write("<h2>"+segundoNumero+" é maior que "+primeiroNumero+"</h2>")
-}
+	}
 
-else if(segundoNumero=primeiroNumero){
+	else if(segundoNumero=primeiroNumero){
 	document.write("<h2>"+primeiroNumero+" é igual a "+segundoNumero+"</h2>")
+	}
+
+	else{
+    alert("O número digitado é nulo, ou houve algum erro, tente novamente")
+	}
 }
 
-else{
-    alert("O número digitado é nulo, ou houve algum erro, tente novamente")
-}
--- scripts/exemplo04.js
+++ scripts/exemplo04.js
@@ -1,8 +1,10 @@
-var multiplicador=parseInt(prompt("Qual número você deseja saber a tabuada:"))
-var contador=0
+function calcular() {
+    var multiplicador = document.getElementById("multiplicador").value
+    var contador = 0
 
-document.write("<h1>Tabuada do "+multiplicador+"</h1>")
-for(contador=0;contador<=10;contador++){
-    resultado=contador*multiplicador
-    document.write("<h2>"+contador+" X "+multiplicador+" = "+resultado+"</h2>")
+    document.write("<h1>Tabuada do " + multiplicador + "</h1>")
+    for (contador = 0; contador <= 10; contador++) {
+        resultado = contador * multiplicador
+        document.write("<h2>" + contador + " X " + multiplicador + " = " + resultado + "</h2>")
+    }
 }
